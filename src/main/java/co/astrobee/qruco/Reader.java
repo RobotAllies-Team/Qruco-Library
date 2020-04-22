569d1e73ef332c2eabe93e1933faccafbcf0950b
Apiphoom Chuenchompoo <apiphoom23@gmail.com>
2020-04-22 11:01:59 +0700

    Update Reader.java

--- src/main/java/co/astrobee/qruco/Reader.java
+++ src/main/java/co/astrobee/qruco/Reader.java
@@ -43,20 +43,9 @@ public interface Reader {
    */
   Result qrcode(BinaryBitmap image) throws NotFoundException, ChecksumException, FormatException;
 
-  /**
-   * Locates and decodes a barcode in some format within an image. This method also accepts
-   * hints, each possibly associated to some data, which may help the implementation decode.
-   *
-   * @param image image of barcode to decode
-   * @param hints passed as a {@link Map} from {@link DecodeHintType}
-   * to arbitrary data. The
-   * meaning of the data depends upon the hint type. The implementation may or may not do
-   * anything with these hints.
-   * @return String which the barcode encodes
-   * @throws NotFoundException if no potential barcode is found
-   * @throws ChecksumException if a potential barcode is found but does not pass its checksum
-   * @throws FormatException if a potential barcode is found but format is invalid
-   */
+  Result qrcode1(BinaryBitmap image) throws Exception;
+  
+  
   Result qrcode(BinaryBitmap image, Map<DecodeHintType,?> hints)
       throws NotFoundException, ChecksumException, FormatException;
 
